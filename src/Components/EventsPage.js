25cf21241257803b5db061252b3cfaaa3010a7f6
Kaleidiana <kaleidiana63@gmail.com>
2024-08-15 23:19:14 +0300

    Uploaded a new page called eventspage

---
+++ src/Components/EventsPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const EventsPage = () => {
+  return (
+    <div className="events-page">
+      <div className="content-area">
+        <h1 className="page-title">All Events in Nairobi</h1>
+        <p className="page-description">
+          Explore all the upcoming events in Nairobi.<br />
+          Find out what’s on in Nairobi in 2024, make it an eventful year, and start exploring right away. 
+          There are countless events in Nairobi from genres like comedy, art, and food to festivals; you can find your pick and have the best time of your life. 
+          Check out some amazing free events in Nairobi to take away all the fun experiences. 
+          The city might offer a lot, but all the events in Nairobi make it unique, distinctive, and different from others. 
+          You can look for all the things to do in Nairobi and soak in the culture and entertainment scene here. Come and have a taste of Nairobi whilst you are here.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default EventsPage;
